5904b3c6297018cb397b83eee93fc21ce341beaa
Nick Drane <nicholasdrane@gmail.com>
2019-08-25 18:02:16 -0600

    set ids on header tags

--- src/components/article/index.jsx
+++ src/components/article/index.jsx
@@ -4,6 +4,7 @@ import styles from "./styles.module.scss";
 import { Link } from "gatsby";
 import Social from "../social";
 import MailChimpForm from "../mailChimpForm";
+import { parse, TextNode, HTMLElement } from "node-html-parser";
 
 export default function Article({
   showSocialTop,
@@ -15,6 +16,8 @@ export default function Article({
   showMailingListForm = false,
   makeTitleClickable = true
 }) {
+  const parsedContent = parse(content);
+  addHeaderIds(parsedContent);
   const month = new Date(date).getUTCMonth();
   const day = new Date(date).getUTCDate();
   const year = new Date(date).getUTCFullYear();
@@ -36,9 +39,35 @@ export default function Article({
           title
         )}
       </h1>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+      <div dangerouslySetInnerHTML={{ __html: parsedContent.toString() }} />
       {showMailingListForm && <MailChimpForm />}
       {showSocialBottom && <Social title={title} url={url} />}
     </article>
   );
+}
+
+const headerTags = ["h1", "h2", "h3", "h4", "h5", "h6"];
+function addHeaderIds(root) {
+  if (root.childNodes.length === 0) {
+    return;
+  }
+  root.childNodes.forEach(child => {
+    if (headerTags.includes(child.tagName)) {
+      const text = child.childNodes[0].text;
+      const id = text
+        .split(" ")
+        .join("-")
+        .toLowerCase();
+      const newNode = new HTMLElement(
+        child.tagName,
+        { id },
+        `id="${id}"`,
+        root
+      );
+      newNode.appendChild(new TextNode(text));
+      root.exchangeChild(child, newNode);
+    } else {
+      addHeaderIds(child);
+    }
+  });
 }
